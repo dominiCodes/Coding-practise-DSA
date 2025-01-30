a2cf50a527564abe42ba5689096178a933cecc6f
Adnan Shaikh <72407647+dominiCodes@users.noreply.github.com>
2025-01-30 21:31:42 +0530

    Time: 201 ms (50%), Space: 41.1 MB (79.92%) - LeetHub

---
+++ 3422-find-the-n-th-value-after-k-seconds/3422-find-the-n-th-value-after-k-seconds.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public int valueAfterKSeconds(int n, int k) {
+        int[] sum = new int[n];
+        Arrays.fill(sum, 1);
+        while(k-- > 0)
+            for(int i = 1 ; i < n ; i++)
+                sum[i] = (sum[i] + sum[i - 1]) % 1000000007;
+        return sum[n - 1];
+    }
+}
